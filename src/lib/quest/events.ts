b49afd9135d695218807cba362dcfb81a30f6437
andrewdepaiva100 <andrewpaiva1000@yahoo.com>
2026-09-08 12:15:38 -0400

    Install global NPC interaction reactions

--- src/lib/quest/events.ts
+++ src/lib/quest/events.ts
@@ -19,7 +19,7 @@ if (typeof window !== "undefined") {
       import("./act2SeasonIdentity"), import("./act2BramForgemaster"), import("./act2BramWeaponOwnership"), import("./act2SeasonChallenges"),
       import("./global2_5dPolish"), import("./desktopActSelector"), import("./desktopControlsPolish"), import("./interactionPolish"),
       import("./wrenIntro"), import("./lastCrossingDialogueRebind"), import("./weaponVisualSync"), import("./introUiPolish"),
-      import("./mariaMovementPolish"), import("./mariaCombatAnimation"),
+      import("./mariaMovementPolish"), import("./mariaCombatAnimation"), import("./globalCharacterReactions"),
     ]).then(([
       sceneModule, houseModule, upgrades, visualRemaster, homeRemaster, homeScalePolish, act1WorldRemaster, lastCrossing, silasPolish,
       silasPlacementFix, lastCrossingLifecycle, act1GameplayPolish, fallenCrossingShrine, fallenCrossingJournalBook,
@@ -27,7 +27,7 @@ if (typeof window !== "undefined") {
       relicPresentation, act2Enemies, act2GardenKeeper, act2EvelynSprite, act2VisualTuning, act2GuideCleanup,
       act2SeasonIdentity, act2BramForgemaster, act2BramWeaponOwnership, act2SeasonChallenges, global25DPolish,
       desktopActSelector, desktopControlsPolish, interactionPolish, wrenIntro, lastCrossingDialogueRebind, weaponVisualSync,
-      introUiPolish, mariaMovementPolish, mariaCombatAnimation,
+      introUiPolish, mariaMovementPolish, mariaCombatAnimation, globalCharacterReactions,
     ]) => {
       const QuestScene = sceneModule.QuestScene as unknown as any;
       const QuestHouseScene = houseModule.QuestHouseScene as unknown as any;
@@ -69,6 +69,7 @@ if (typeof window !== "undefined") {
       introUiPolish.installIntroUiPolish(QuestScene);
       mariaMovementPolish.installMariaMovementPolish(QuestScene, QuestHouseScene);
       mariaCombatAnimation.installMariaCombatAnimation(QuestScene);
+      globalCharacterReactions.installGlobalCharacterReactions(QuestScene);
 
       // Let Phaser/browser complete two paints before parsing and installing the
       // large dialogue script. This removes it from the first playable frame.
