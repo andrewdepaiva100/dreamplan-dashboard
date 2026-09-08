2988a808fd72e6cf3c21bca8fb28faa0d3b5cc15
andrewdepaiva100 <andrewpaiva1000@yahoo.com>
2026-09-08 04:13:02 -0400

    Install global Maria movement polish

--- src/lib/quest/events.ts
+++ src/lib/quest/events.ts
@@ -19,6 +19,7 @@ if (typeof window !== "undefined") {
       import("./act2SeasonIdentity"), import("./act2BramForgemaster"), import("./act2BramWeaponOwnership"), import("./act2SeasonChallenges"),
       import("./global2_5dPolish"), import("./desktopActSelector"), import("./desktopControlsPolish"), import("./interactionPolish"),
       import("./wrenIntro"), import("./lastCrossingDialogueRebind"), import("./weaponVisualSync"), import("./introUiPolish"),
+      import("./mariaMovementPolish"),
     ]).then(([
       sceneModule, houseModule, upgrades, visualRemaster, homeRemaster, homeScalePolish, act1WorldRemaster, lastCrossing, silasPolish,
       silasPlacementFix, lastCrossingLifecycle, act1GameplayPolish, fallenCrossingShrine, fallenCrossingJournalBook,
@@ -26,7 +27,7 @@ if (typeof window !== "undefined") {
       relicPresentation, act2Enemies, act2GardenKeeper, act2EvelynSprite, act2VisualTuning, act2GuideCleanup,
       act2SeasonIdentity, act2BramForgemaster, act2BramWeaponOwnership, act2SeasonChallenges, global25DPolish,
       desktopActSelector, desktopControlsPolish, interactionPolish, wrenIntro, lastCrossingDialogueRebind, weaponVisualSync,
-      introUiPolish,
+      introUiPolish, mariaMovementPolish,
     ]) => {
       const QuestScene = sceneModule.QuestScene as unknown as any;
       const QuestHouseScene = houseModule.QuestHouseScene as unknown as any;
@@ -66,6 +67,7 @@ if (typeof window !== "undefined") {
       lastCrossingDialogueRebind.rebindLastCrossingDialoguePolish(QuestScene);
       weaponVisualSync.installWeaponVisualSync(QuestScene);
       introUiPolish.installIntroUiPolish(QuestScene);
+      mariaMovementPolish.installMariaMovementPolish(QuestScene, QuestHouseScene);
 
       // Let Phaser/browser complete two paints before parsing and installing the
       // large dialogue script. This removes it from the first playable frame.
