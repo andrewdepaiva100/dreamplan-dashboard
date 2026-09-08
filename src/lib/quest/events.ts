a14baeb3dd5e3e0ed7917cc97d2c6edc08c9a9bc
andrewdepaiva100 <andrewpaiva1000@yahoo.com>
2026-09-08 13:58:40 -0400

    Install environmental micro-life ambience

--- src/lib/quest/events.ts
+++ src/lib/quest/events.ts
@@ -20,7 +20,7 @@ if (typeof window !== "undefined") {
       import("./global2_5dPolish"), import("./desktopActSelector"), import("./desktopControlsPolish"), import("./interactionPolish"),
       import("./wrenIntro"), import("./lastCrossingDialogueRebind"), import("./weaponVisualSync"), import("./introUiPolish"),
       import("./mariaMovementPolish"), import("./mariaCombatAnimation"), import("./globalCharacterReactions"), import("./enemyReactionPolish"),
-      import("./actArrivalCinematics"),
+      import("./actArrivalCinematics"), import("./environmentalMicroLife"),
     ]).then(([
       sceneModule, houseModule, upgrades, visualRemaster, homeRemaster, homeScalePolish, act1WorldRemaster, lastCrossing, silasPolish,
       silasPlacementFix, lastCrossingLifecycle, act1GameplayPolish, fallenCrossingShrine, fallenCrossingJournalBook,
@@ -29,6 +29,7 @@ if (typeof window !== "undefined") {
       act2SeasonIdentity, act2BramForgemaster, act2BramWeaponOwnership, act2SeasonChallenges, global25DPolish,
       desktopActSelector, desktopControlsPolish, interactionPolish, wrenIntro, lastCrossingDialogueRebind, weaponVisualSync,
       introUiPolish, mariaMovementPolish, mariaCombatAnimation, globalCharacterReactions, enemyReactionPolish, actArrivalCinematics,
+      environmentalMicroLife,
     ]) => {
       const QuestScene = sceneModule.QuestScene as unknown as any;
       const QuestHouseScene = houseModule.QuestHouseScene as unknown as any;
@@ -73,6 +74,7 @@ if (typeof window !== "undefined") {
       globalCharacterReactions.installGlobalCharacterReactions(QuestScene);
       enemyReactionPolish.installEnemyReactionPolish(QuestScene);
       actArrivalCinematics.installActArrivalCinematics(QuestScene);
+      environmentalMicroLife.installEnvironmentalMicroLife(QuestScene);
 
       // Let Phaser/browser complete two paints before parsing and installing the
       // large dialogue script. This removes it from the first playable frame.
